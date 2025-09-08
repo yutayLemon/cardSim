c0aa6d60ee7da8416642166b311b0f7efe5de824
yutay.lemon <yutay.lemon@gmail.com>
2025-09-08 18:48:12 +1000

    a

---
+++ yutaWorlds/src/physicalObj.js
@@ -0,0 +1,23 @@
+class worldObj{
+    constructor(){
+        this.pos;
+        this.Global;
+        this.name;
+        this.threeObj = [];
+        this.bottomCenter;
+    }
+}
+
+class blockObj extends worldObj{
+    constructor(){
+        super();
+
+    }
+}
+
+class scene extends worldObj{
+    constructor(){
+        super();
+        
+    }
+}
