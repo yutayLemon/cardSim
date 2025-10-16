d7dbb3c4cda1fb5eca0a7030efd7d900bb68a193
yutay.lemon <yutay.lemon@gmail.com>
2025-10-17 01:51:03 +1100

    pre
    befor implemenation of half mesh data strcuture

--- yutaWorlds/src/boxObj.js
+++ yutaWorlds/src/boxObj.js
@@ -29,7 +29,7 @@ class boxObj{
         this.rotationMatrx = new THREE.Matrix3(
             1,0,0,
             0,1,0,
-            0,0,1
+            0,0,1 
         );
         this.correctionRotationMatrx = new THREE.Matrix3(
             1,0,0,
