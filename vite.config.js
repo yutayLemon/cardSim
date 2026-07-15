4c21e6751d0d08b4811461431d38ce24eda07c08
yutay.lemon <yuta.yamashita@student.unimelb.edu.au>
2026-07-16 00:55:56 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // or vue(), etc.
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/cardSim', // Must match your exact repository name with slashes
+  // Adjust 'your-repo-name' to match your GitHub repository name!
+  base: '/cardSim/', 
 })
