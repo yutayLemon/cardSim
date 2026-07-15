78cdc1665dab273ba23245204737997a4197a533
yutay.lemon <yuta.yamashita@student.unimelb.edu.au>
2026-07-15 19:20:40 +1000

    hbhh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react' // or vue(), etc.
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repository-name/', // Must match your exact repository name with slashes
+  base: '/cardSim', // Must match your exact repository name with slashes
 })
